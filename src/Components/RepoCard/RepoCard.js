70792760e8ec9d37b88aa13813799a0c664694c4
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-11-10 05:22:17 +0600

    Repo card updated

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "../UI/Button/Button";
 const { default: SimpleCard } = require("../UI/SimpleCard/SimpleCard");
 
-const RepoCard = (props) => {
-	return <SimpleCard>Repo Card: {props.repo.name}</SimpleCard>;
-};
+class RepoCard extends Component {
+	state = {
+		isExpanded: false,
+	};
+
+	render() {
+		let commits = <div>No Commit History</div>;
+
+		if (this.props.repo.commits != null)
+			commits = this.props.repo.commits.map((commit) => {
+				return (
+					<div>
+						<div>
+							<b>Commiter:</b> {commit.name}
+						</div>
+						<div>
+							<b>TimeStamp</b> {commit.timestamp}
+						</div>
+						<br />
+					</div>
+				);
+			});
+		let expandedSection = (
+			<div>
+				<Button
+					buttonType="Danger"
+					onClick={() => {
+						this.setState({ isExpanded: false });
+					}}
+				>
+					Hide
+				</Button>
+				{commits}
+			</div>
+		);
+
+		if (!this.state.isExpanded)
+			expandedSection = (
+				<div>
+					<Button
+						buttonType="Success"
+						onClick={() => {
+							this.setState({ isExpanded: true });
+						}}
+					>
+						Show
+					</Button>
+				</div>
+			);
+
+		return (
+			<SimpleCard>
+				<div>name: {this.props.repo.name}</div>
+				{expandedSection}
+			</SimpleCard>
+		);
+	}
+}
 
 export default RepoCard;
--- src/store/reducers/collection.js
+++ src/store/reducers/collection.js
@@ -3,21 +3,53 @@ import * as actionTypes from "../actions/actionTypes";
 const dummyRepo1 = {
 	id: 1,
 	name: "dummyRepo1",
+	occupied: true,
+	commits: [
+		{
+			name: "dummyUser1",
+			timestamp: 1,
+		},
+		{
+			name: "dummyUser1",
+			timestamp: 2,
+		},
+		{
+			name: "dummyUser1",
+			timestamp: 2,
+		},
+	],
 };
 
 const dummyRepo2 = {
 	id: 2,
 	name: "dummyRepo2",
+	occupied: true,
+	commits: [
+		{
+			name: "dummyUser2",
+			timestamp: 4,
+		},
+		{
+			name: "dummyUser2",
+			timestamp: 5,
+		},
+		{
+			name: "dummyUser2",
+			timestamp: 6,
+		},
+	],
 };
 
 const dummyRepo3 = {
 	id: 3,
 	name: "dummyRepo3",
+	occupied: true,
 };
 
 const dummyRepo4 = {
 	id: 4,
 	name: "dummyRepo4",
+	occupied: false,
 };
 
 const dummyCollection1 = {
