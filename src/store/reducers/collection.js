3f742b5fa692df86fe8e64eff43effdb5aa9b1c7
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-11-13 13:09:05 +0600

    Removed dummy data

--- src/store/reducers/collection.js
+++ src/store/reducers/collection.js
@@ -1,76 +1,8 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const dummyRepo1 = {
-	id: 1,
-	name: "dummyRepo1",
-	occupied: true,
-	commits: [
-		{
-			name: "dummyUser1",
-			timestamp: 1,
-		},
-		{
-			name: "dummyUser1",
-			timestamp: 2,
-		},
-		{
-			name: "dummyUser1",
-			timestamp: 2,
-		},
-	],
-};
-
-const dummyRepo2 = {
-	id: 2,
-	name: "dummyRepo2",
-	occupied: true,
-	commits: [
-		{
-			name: "dummyUser2",
-			timestamp: 4,
-		},
-		{
-			name: "dummyUser2",
-			timestamp: 5,
-		},
-		{
-			name: "dummyUser2",
-			timestamp: 6,
-		},
-	],
-};
-
-const dummyRepo3 = {
-	id: 3,
-	name: "dummyRepo3",
-	occupied: true,
-};
-
-const dummyRepo4 = {
-	id: 4,
-	name: "dummyRepo4",
-	occupied: false,
-};
-
-const dummyCollection1 = {
-	id: 1,
-	name: "DummyCollection1",
-	type: "DummyType1",
-	createdAt: 1605015702000,
-	repos: [dummyRepo1, dummyRepo2],
-};
-
-const dummyCollection2 = {
-	id: 2,
-	name: "DummyCollection2",
-	type: "DummyType2",
-	createdAt: 1605019702000,
-	repos: [dummyRepo3],
-};
-
 const initialState = {
-	collections: [dummyCollection1, dummyCollection2],
-	repos: [dummyRepo1, dummyRepo2, dummyRepo3, dummyRepo4],
+	collections: [],
+	repos: [],
 	pending: false,
 };
 
